d97e9194823e405bca90dd5747c9df2d3a6c03c3
Tiago Cunha <tiago92cunha@gmail.com>
2025-04-23 20:21:46 +0100

    Add a loader to recently generated recipes in the home page

--- src/modules/home/ui/sections/recent-recipes-section.tsx
+++ src/modules/home/ui/sections/recent-recipes-section.tsx
@@ -2,10 +2,47 @@
 import React, { Suspense } from "react";
 import RecentRecipes from "../components/recent-recipes";
 import { api } from "@/trpc/react";
+import { Card, CardFooter } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { CardContent } from "@/components/ui/card";
+
+const Loading = () => (
+  <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    {Array(3)
+      .fill(0)
+      .map((_, i) => (
+        <Card key={i} className="h-full overflow-hidden py-0">
+          <Skeleton className="h-48 w-full" />
+          <CardContent className="p-4">
+            <div className="mb-2 flex items-center gap-2">
+              <Skeleton className="h-6 w-6 rounded-full" />
+              <Skeleton className="h-4 w-24" />
+              <Skeleton className="ml-auto h-4 w-16" />
+            </div>
+            <Skeleton className="mb-2 h-6 w-full" />
+            <Skeleton className="mb-4 h-6 w-3/4" />
+            <div className="flex items-center gap-2">
+              <Skeleton className="h-6 w-20 rounded-full" />
+              <Skeleton className="h-4 w-16" />
+            </div>
+          </CardContent>
+          <CardFooter className="p-4 pt-0">
+            <Skeleton className="h-4 w-32" />
+          </CardFooter>
+        </Card>
+      ))}
+  </div>
+);
 
 export function RecentRecipesSection() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <PageStructure>
+          <Loading />
+        </PageStructure>
+      }
+    >
       <RecentRecipesSectionSuspense />
     </Suspense>
   );
@@ -17,6 +54,14 @@ function RecentRecipesSectionSuspense() {
   });
 
   return (
+    <PageStructure>
+      <RecentRecipes recipes={data.items} />
+    </PageStructure>
+  );
+}
+
+const PageStructure = ({ children }: { children: React.ReactNode }) => {
+  return (
     <section className="bg-muted py-12 md:py-16">
       <div className="container px-4 md:px-6">
         <div className="mb-8 space-y-2">
@@ -27,8 +72,8 @@ function RecentRecipesSectionSuspense() {
             Check out these recipes recently created by our users
           </p>
         </div>
-        <RecentRecipes recipes={data.items} />
+        {children}
       </div>
     </section>
   );
-}
+};
